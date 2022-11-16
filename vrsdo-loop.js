a0c005286457dc6c1e3eaf5cd8ffc7a093372dc2
Ted Goddard <ted.goddard@robotsandpencils.com>
2022-11-15 20:34:11 -0700

    rough looping VR stereo

---
+++ vrsdo-loop.js
@@ -0,0 +1,131 @@
+import { Vector3 } from "./js/three.module.js"
+import { VRRoom, ivory, logFlash } from "./vrroom.js"
+
+const vrRoom = new VRRoom({ disableBackground: true, disableGrid: true })
+const scene = vrRoom.scene
+const THREE = vrRoom.THREE
+let currentSelect = null
+let video1 = null
+let video2 = null
+let screen1 = null
+let screen2 = null
+let source1 = null
+let source2 = null
+const buttons = []
+
+async function loadImageButton(image, options) {
+  const mesh = await vrRoom.loadTexturePanel(image)
+  // mesh.rotation.x = -vrRoom.halfPi
+  mesh.position.copy(options.position)
+  mesh.scale.copy(options.scale)
+  scene.add(mesh)
+  return mesh
+}
+
+function loadVideo() {
+  const geometry = new THREE.PlaneGeometry(2, 2)
+
+  video1 = document.getElementById('video1')
+  source1 = document.getElementById('source1')
+  const texture1 = new THREE.VideoTexture(video1)
+  const material1 = new THREE.MeshBasicMaterial({ map: texture1 })
+  screen1 = new THREE.Mesh(geometry, material1)
+  // screen1.layers.set(1)
+  screen1.position.set(0, 1, -2)
+  scene.add(screen1)
+
+  video2 = document.getElementById('video2')
+  source2 = document.getElementById('source2')
+  const texture2 = new THREE.VideoTexture(video2)
+  const material2 = new THREE.MeshBasicMaterial({ map: texture2 })
+  screen2 = new THREE.Mesh(geometry, material2)
+  screen2.layers.set(2)
+  screen2.position.set(0, 1, -2)
+  scene.add(screen2)
+
+  document.addEventListener( 'click', function () {
+    video1.play()
+    video2.play()
+    video1.currentTime = video2.currentTime + 1
+  } )
+}
+
+const channels = [
+  "0094", "0131", "0171", "0193", "0211", "0304", "0335", "1600", "1700"
+]
+
+const buttonDefaults = {
+  scale: new Vector3(0.04, 0.04, 1),
+}
+
+async function initButtons() {
+  let i = 0
+  for (let x of [1.5, 2, 2.5]) {
+    for (let y of [1.5, 1, 0.5]) {
+      const channel = channels[i++]
+      
+      const button = await loadImageButton(`images/sdo_${channel}.jpg`, 
+        { ...buttonDefaults, position: new Vector3(x, y, 0) })
+      button.userData.event = channel
+      vrRoom.pointerUpObjects.push(button)
+      buttons.push(button)
+    }
+  }
+
+}
+
+async function doEvent(target) {
+  const event = target?.userData?.event
+  const handler = eventHandlers[event]
+  if (handler) {
+    handler()
+    return
+  }
+
+  const channel = event
+  console.log("pointerUp", {channel, isImageMode})
+  const src = `../sdo/sample-${channel}.mp4`
+  source1.src = src
+  source2.src = src
+  video1.load()
+  video2.load()
+  console.log(source1)
+}
+
+vrRoom.onSessionStarted((session) => {
+  screen1.layers.set(1)
+})
+
+vrRoom.onPointerUp( async (position, intersects) => {
+  const target = intersects[0]?.object
+  await doEvent(target)
+})
+
+vrRoom.addSelectListener(async () => {
+  const target = currentSelect?.object?.userData?.object
+  await doEvent(target)
+})
+
+function pointerListener(hits) {
+  currentSelect = hits[0][0] || hits[1][0]
+  if (currentSelect) {
+    const object = currentSelect.object.userData.object
+    const highlightObject = object.userData.highlightObject ?? object
+    let material = highlightObject.material
+    material = material[1] || material
+    if (!highlightObject.userData.color) {
+      // object.userData.color = material.color
+      highlightObject.userData.color = { r: 0.53333, g: 0.53333, b: 0.53333}
+    }
+    material.color.r = 1
+    setTimeout(() => { material.color.r = highlightObject.userData.color.r }, 100)
+  }
+}
+
+async function init() {
+  await initButtons()
+  loadVideo()
+  vrRoom.addPointerListener([...buttons], pointerListener)
+}
+
+init().then()
