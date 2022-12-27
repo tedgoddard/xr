3024aa68a3d11ee9eaf62ec3449a851f434760cf
Ted Goddard <ted.goddard@robotsandpencils.com>
2022-12-26 20:18:30 -0700

    use PointsMaterial

--- pointe.js
+++ pointe.js
@@ -1,5 +1,6 @@
 import { VRRoom, logFlash } from "./vrroom.js"
 import * as THREE from './js/three.module.js';
+import * as BufferGeometryUtils from './jsm/utils/BufferGeometryUtils.js'
 
 const cameraOptions = { fov: 50, near: 0.01, far: 1000 }
 const vrRoom = new VRRoom({ disableBackground: true, disableGrid: true, camera: cameraOptions })
@@ -14,8 +15,9 @@ async function sleep(millis) {
 }
 
 async function loadFloor() {
-  const geometry = new THREE.RingGeometry(1.95, 2, 32)
-  geometry.merge(new THREE.RingGeometry(0.95, 1, 32))
+  const ring1 = new THREE.RingGeometry(1.95, 2, 32)
+  const ring2 = new THREE.RingGeometry(0.95, 1, 32)
+  const geometry = BufferGeometryUtils.mergeBufferGeometries([ring1, ring2])
   const material = new THREE.MeshBasicMaterial({ color: 0x660000, side: THREE.DoubleSide })
   const mesh = new THREE.Mesh(geometry, material)
   mesh.rotation.x = vrRoom.halfPi
@@ -23,20 +25,35 @@ async function loadFloor() {
 }
 
 function buildModel(model) {
+
+  const positions = []
+  const colors = []
+
   const { coords, channels } = model
-  const modelGroup = new THREE.Group()
   for (let i = 0; i < coords.length; i++) {
-    const boxGeom = new THREE.TetrahedronGeometry(0.005)
     const r = channels.R[i]
     const g = channels.G[i]
     const b = channels.B[i]
-    const color = new THREE.Color(r, g, b)
-    const material = new THREE.MeshBasicMaterial( {color } )
-    const box = new THREE.Mesh(boxGeom, material)
-    box.position.set(...coords[i])
-    modelGroup.add(box)
+    colors.push(r, g, b)
+    positions.push(...coords[i])
   }
-  return modelGroup
+
+  const geometry = new THREE.BufferGeometry()
+
+  if (positions.length > 0) geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions,3))
+  // if ( normal.length > 0 ) geometry.setAttribute( 'normal', new Float32BufferAttribute( normal, 3 ) );
+  if (colors.length > 0) geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+  // if ( intensity.length > 0 ) geometry.setAttribute( 'intensity', new Float32BufferAttribute( intensity, 1 ) );
+  // if ( label.length > 0 ) geometry.setAttribute( 'label', new Int32BufferAttribute( label, 1 ) );
+
+  geometry.computeBoundingSphere()
+  const material = new THREE.PointsMaterial({ size: 0.01 })
+
+  if (colors.length > 0) {
+    material.vertexColors = true
+  }
+
+  return new THREE.Points(geometry, material)
 }
 
 async function init() {
